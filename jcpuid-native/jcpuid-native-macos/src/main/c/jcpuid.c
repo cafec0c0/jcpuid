637eeb938a9e1a894c02f4051f94305808838223
Adam Bruce <adam@adambruce.net>
2025-03-03 18:30:52 +0000

    Add macos build

---
+++ jcpuid-native/jcpuid-native-macos/src/main/c/jcpuid.c
@@ -0,0 +1,23 @@
+#include <cpuid.h>
+
+#include "net_adambruce_bridge_Amd64CPUIDBridgeImpl.h"
+
+JNIEXPORT jobject JNICALL Java_net_adambruce_bridge_Amd64CPUIDBridgeImpl_getCPUIDNative
+  (JNIEnv *env, jobject jobj, jint leaf) {
+    // Class of return object
+    jclass clazz = (*env)->FindClass(env, "net/adambruce/type/Result");
+
+    // Constructor ID
+    jmethodID cid = (*env)->GetMethodID(env, clazz, "<init>", "(IIIII)V");
+
+    unsigned int status;
+    unsigned int eax;
+    unsigned int ebx;
+    unsigned int ecx;
+    unsigned int edx;
+    status = __get_cpuid(leaf, &eax, &ebx, &ecx, &edx);
+
+    // Create new CPUIDResult
+    jobject newobj = (*env)->NewObject(env, clazz, cid, status, eax, ebx, ecx, edx);
+    return newobj;
+}
