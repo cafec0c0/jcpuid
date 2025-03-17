511a61f1e9ad32ed7c2e55068564977f87a68d83
Adam Bruce <adam@adambruce.net>
2025-03-17 21:54:30 +0000

    Update example to print features

--- jcpuid-examples/jcpuid-example-print-all-info/src/main/java/net/adambruce/jcpuid/PrintAllCPUIDInformation.java
+++ jcpuid-examples/jcpuid-example-print-all-info/src/main/java/net/adambruce/jcpuid/PrintAllCPUIDInformation.java
@@ -49,12 +49,12 @@ public class PrintAllCPUIDInformation {
 
     private static void printLargestStandardFunctionNumber(CPUID cpuid) {
         int largestInstructionNum = cpuid.getLargestStandardFunctionNumber();
-        System.out.println("Largest Standard Function Number: " + largestInstructionNum);
+        System.out.printf("%-32s: %d%n", "Largest Standard Function Number", largestInstructionNum);
     }
 
     private static void printProcessorVendor(CPUID cpuid) {
         String vendor = cpuid.getProcessorVendor();
-        System.out.println("Vendor: " + vendor);
+        System.out.printf("%-32s: %s%n", "Vendor", vendor);
     }
 
     private static class IntelCPUIDPrinter {
@@ -68,22 +68,22 @@ public class PrintAllCPUIDInformation {
         }
 
         private static void printVersionInformation(IntelCPUID cpuid) {
-            System.out.println("Version Information:");
+            System.out.printf("%-32s%n", "Version Information");
             try {
                 VersionInformation version = cpuid.getVersionInformation();
-                System.out.println("  Stepping       : " + version.getStepping());
-                System.out.println("  Model          : " + version.getModel());
-                System.out.println("  Family         : " + version.getFamily());
-                System.out.println("  Processor Type : " + version.getType());
-                System.out.println("  Extended Model : " + version.getExtendedModel());
-                System.out.println("  Extended Family: " + version.getExtendedFamily());
+                System.out.printf("  %-30s: %d%n", "Stepping", version.getStepping());
+                System.out.printf("  %-30s: %d%n", "Model", version.getModel());
+                System.out.printf("  %-30s: %d%n", "Family", version.getFamily());
+                System.out.printf("  %-30s: %s%n", "Type", version.getType());
+                System.out.printf("  %-30s: %d%n", "Extended Model", version.getExtendedModel());
+                System.out.printf("  %-30s: %d%n", "Extended Family", version.getExtendedFamily());
             } catch (CPUIDException ex) {
                 System.out.println("  Exception: " + ex);
             }
         }
 
         private static void printBrandIndex(IntelCPUID cpuid) {
-            System.out.print("Brand String Index: ");
+            System.out.printf("%-32s: ", "Brand String Index");
             try {
                 System.out.println(cpuid.getBrandIndex());
             } catch (CPUIDException ex) {
@@ -92,7 +92,7 @@ public class PrintAllCPUIDInformation {
         }
 
         private static void printCLFLUSHLineSize(IntelCPUID cpuid) {
-            System.out.print("CLFLUSH Line Size: ");
+            System.out.printf("%-32s: ", "CLFLUSH Line Size");
             try {
                 int size = cpuid.getCLFLUSHLineSize();
                 System.out.println(size + " (" + (size * 8) + " bytes)");
@@ -102,7 +102,7 @@ public class PrintAllCPUIDInformation {
         }
 
         private static void printMaxNumberOfAddressableIds(IntelCPUID cpuid) {
-            System.out.print("Maximum Number of Addressable IDs: ");
+            System.out.printf("%-32s: ", "Max Num of Addressable IDs");
             try {
                 System.out.println(cpuid.getMaximumNumberOfAddressableIds());
             } catch (CPUIDException ex) {
@@ -111,7 +111,7 @@ public class PrintAllCPUIDInformation {
         }
 
         private static void printInitialApicId(IntelCPUID cpuid) {
-            System.out.print("Initial APIC ID: ");
+            System.out.printf("%-32s: ", "Initial APIC ID");
             try {
                 System.out.println(cpuid.getInitialApicId());
             } catch (CPUIDException ex) {
@@ -120,10 +120,74 @@ public class PrintAllCPUIDInformation {
         }
 
         private static void printFeatureInformation(IntelCPUID cpuid) {
-            System.out.println("Feature Information: ");
+            System.out.printf("%-32s: ", "Features");
             try {
                 FeatureInformation info = cpuid.getFeatureInformation();
-                System.out.println("  " + String.join("\n  ", info.toString().split("\\{")[1].split("}")[0].replace("=", ": ").split(",")));
+                StringBuilder builder = new StringBuilder();
+                if (info.getSSE3()) builder.append("SSE3,");
+                if (info.getPCLMULQDQ()) builder.append("PCLMULQDQ,");
+                if (info.getDTES64()) builder.append("DTES64,");
+                if (info.getDSCPL()) builder.append("DSCPL,");
+                if (info.getVMX()) builder.append("VMX,");
+                if (info.getSMX()) builder.append("SMX,");
+                if (info.getEIST()) builder.append("EIST,");
+                if (info.getTM2()) builder.append("TM2,");
+                if (info.getSSSE3()) builder.append("SSSE3,");
+                if (info.getCNXTID()) builder.append("CNXT-ID,");
+                if (info.getSDBG()) builder.append("SDBG,");
+                if (info.getFMA()) builder.append("FMA,");
+                if (info.getCMPXCHG16B()) builder.append("CMPXCHG16B,");
+                if (info.getXTPRUpdateControl()) builder.append("xTPR Update Control,");
+                if (info.getPDCM()) builder.append("PDCM,");
+                if (info.getPCID()) builder.append("PCID,");
+                if (info.getDCA()) builder.append("DCA,");
+                if (info.getSSE41()) builder.append("SSE4_1,");
+                if (info.getSSE42()) builder.append("SSE4_2,");
+                if (info.getX2APIC()) builder.append("x2APIC,");
+                if (info.getMOVBE()) builder.append("MOVBE,");
+                if (info.getPOPCNT()) builder.append("POPCNT,");
+                if (info.getTSCDeadline()) builder.append("TSC-Deadline,");
+                if (info.getAESNI()) builder.append("AESNI,");
+                if (info.getXSAVE()) builder.append("XSAVE,");
+                if (info.getOSXSAVE()) builder.append("OSXSAVE,");
+                if (info.getAVX()) builder.append("AVX,");
+                if (info.getF16C()) builder.append("F16C,");
+                if (info.getRDRAND()) builder.append("RDRAND,");
+
+                if (info.getFPU()) builder.append("FPU,");
+                if (info.getVME()) builder.append("VME,");
+                if (info.getDE()) builder.append("DE,");
+                if (info.getPSE()) builder.append("PSE,");
+                if (info.getTSC()) builder.append("TSC,");
+                if (info.getMSR()) builder.append("MSR,");
+                if (info.getPAE()) builder.append("PAE,");
+                if (info.getMCE()) builder.append("MCE,");
+                if (info.getCX8()) builder.append("CX8,");
+                if (info.getAPIC()) builder.append("APIC,");
+                if (info.getSEP()) builder.append("SEP,");
+                if (info.getMTRR()) builder.append("MTRR,");
+                if (info.getPGE()) builder.append("PGE,");
+                if (info.getMCA()) builder.append("MCA,");
+                if (info.getCMOV()) builder.append("CMOV,");
+                if (info.getPAT()) builder.append("PAT,");
+                if (info.getPSE36()) builder.append("PSE-36,");
+                if (info.getPSN()) builder.append("PSN,");
+                if (info.getCLFSH()) builder.append("CLFSH,");
+                if (info.getDS()) builder.append("DS,");
+                if (info.getACPI()) builder.append("ACPI,");
+                if (info.getMMX()) builder.append("MMX,");
+                if (info.getFXSR()) builder.append("FXSR,");
+                if (info.getSSE()) builder.append("SSE,");
+                if (info.getSSE2()) builder.append("SSE2,");
+                if (info.getSS()) builder.append("SS,");
+                if (info.getHTT()) builder.append("HTT,");
+                if (info.getTM()) builder.append("TM,");
+                if (info.getPBE()) builder.append("PBE,");
+
+                if (builder.toString().endsWith(","))
+                    System.out.println(builder.substring(0, builder.length() - 1));
+                else
+                    System.out.println(builder);
             } catch (CPUIDException ex) {
                 System.out.println("Exception: " + ex);
             }
